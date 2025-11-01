18ac7c7c9cad36b251189f4c7053fc2aa859c635
Ivan Lambrev <ilambrev@gmail.com>
2025-11-01 12:03:18 +0200

    You Can (not) Escape

---
+++ 2025_10_30/2025_10_30_you_can_not_escape.py
@@ -0,0 +1,65 @@
+from collections import deque
+
+file_path = "hyperskill-dataset-117518495.txt"
+
+file = open(file_path, "r")
+map = file.read().strip().split("\n")
+file.close()
+
+start_point_sign = "P"
+destination_point_sign = "G"
+wall_sign = "#"
+walkable_floor_sign = "."
+visited_floor_sign = "V"
+destinations = {0: "U", 1: "D", 2: "L", 3: "R"}
+
+def is_cell_valid(row, col, n, m, mat, visited):
+
+    return (row >= 0 and row < n and 
+           col >= 0 and col < m and 
+           mat[row][col] != wall_sign and 
+           not visited[row][col] == visited_floor_sign)
+
+def find_point_coorinates(mat, point_sign):
+    for i in range(len(mat)):
+        for j in range(len(mat[i])):
+            if mat[i][j] == point_sign:
+                return [i, j]
+            
+def shortest_path(mat):
+    n = len(map)
+    m = len(map[0])
+    p = find_point_coorinates(mat, start_point_sign)
+    dRow = [-1, 1, 0, 0]
+    dCol = [0, 0, -1, 1]
+
+    visited = [[walkable_floor_sign for _ in range(m)] for _ in range(n)]
+
+    q = deque()
+
+    q.append([p[0], p[1], ""])
+
+    visited[p[0]][p[1]] = visited_floor_sign
+
+    while q:
+        curr = q.popleft()
+
+        row = curr[0]
+        col = curr[1]
+        dist = curr[2]
+
+        if mat[row][col] == destination_point_sign:
+            return dist
+
+        for i in range(4):
+            currenr_destination = destinations[i]
+            newRow = row + dRow[i]
+            newCol = col + dCol[i]
+
+            if is_cell_valid(newRow, newCol, n, m, mat, visited):
+                visited[newRow][newCol] = visited_floor_sign
+                q.append([newRow, newCol, dist + currenr_destination])
+
+    return -1
+
+print(shortest_path(map))
